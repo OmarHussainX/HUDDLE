8eb4a025257cd1eec9a1a18d411987b77d817aa0
Joe Dang <mrjoedang@gmail.com>
2019-03-25 21:17:28 -0600

    set up basic routes in index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import Login from './Login'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/" component={App} exact={true} />
+        <Route path="/login" component={Login} exact={true} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
